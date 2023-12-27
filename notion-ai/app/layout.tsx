e1bd1d72b0e5797b83c2c39377ecdf653f286748
Aankirz <sahuankit453@gmail.com>
2023-12-27 15:24:54 +0530

    basic setup and landingpage

---
+++ notion-ai/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Neural Note",
+  description: "Notion + Langchain LLMs",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="jotion-theme-2"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ notion-ai/convex/auth.config.js
@@ -0,0 +1,9 @@
+//
+export default {
+  providers: [
+    {
+      domain: "https://expert-redbird-38.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
